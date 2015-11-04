d9c4311acc7571bec08bf1c2577e751ca8bdc99e
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2015-11-04 17:55:10 +0100

    Big Update
    Textures Update

---
+++ Guardian Angel/Assets/Code/Player/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float Health;
+
+	void OnGUI(){
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		if (collision.gameObject.tag == "Enemy") {
+			Health = Health - 1;
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
